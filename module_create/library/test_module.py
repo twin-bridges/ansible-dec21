66e48ba730c721e20ea7a75186d99977326efbbe
Kirk Byers <ktbyers@twb-tech.com>
2021-12-18 21:21:17 -0800

    Day4 collateral

---
+++ module_create/library/test_module.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+def main():
+
+    # Define your modules arguments
+    module_args = dict(
+        name=dict(type="str", required=True),
+        new=dict(type="bool", required=False, default=False),
+    )
+
+    # Create an instance of the AnsibleModule class
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
+
+    # Define standard results
+    result = {
+        "changed": False,
+        "original_message": "Something",
+        "message": f"name: {module.params['name']}; new: {module.params['new']}...it worked!!!"
+    }
+
+    # Return items as JSON
+    module.exit_json(**result)
+
+
+if __name__ == "__main__":
+    main()
