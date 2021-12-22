c466c47325632d036c6d0b0ac60ef923692b3297
Kirk Byers <ktbyers@twb-tech.com>
2021-12-21 20:56:39 -0800

    Day4 exercises

--- day4/dynamic_inventory/dyn_inv.py
+++ day4/dynamic_inventory/dyn_inv.py
@@ -10,8 +10,7 @@ ANSIBLE_INV = {
     "all": {
         "vars": {
             "ansible_connection": "network_cli",
-            "ansible_python_interpreter": "~/VENV/ansible/bin/python",
-            "ansible_user": "pyclass",
+            "ansible_python_interpreter": "/home/student5/VENV/ansible/bin/python",
             "ansible_user": "pyclass",
         },
     },
@@ -19,14 +18,32 @@ ANSIBLE_INV = {
         "hosts": ["arista5", "arista6", "arista7", "arista8"],
         "vars": {"ansible_network_os": "eos"},
     },
+    "cisco": {
+        "hosts": ["cisco1", "cisco2", "cisco5", "cisco6"],
+        "vars": {"ansible_network_os": "ios"},
+    },
+    "juniper": {
+        "hosts": ["vmx1", "vmx2"],
+        "vars": {"ansible_network_os": "junos", "ansible_connection": "netconf"},
+    },
+    "nxos": {"hosts": ["nxos1", "nxos2"], "vars": {"ansible_network_os": "nxos"}},
     "local": {"hosts": ["localhost"], "vars": {"ansible_connection": "local"}},
 }
+
 
 HOST_VARS = {
     "arista5": {"ansible_host": "arista5.lasthop.io"},
     "arista6": {"ansible_host": "arista6.lasthop.io"},
     "arista7": {"ansible_host": "arista7.lasthop.io"},
     "arista8": {"ansible_host": "arista8.lasthop.io"},
+    "cisco1": {"ansible_host": "cisco1.lasthop.io"},
+    "cisco2": {"ansible_host": "cisco2.lasthop.io"},
+    "cisco5": {"ansible_host": "cisco5.lasthop.io"},
+    "cisco6": {"ansible_host": "cisco6.lasthop.io"},
+    "vmx1": {"ansible_host": "vmx1.lasthop.io"},
+    "vmx2": {"ansible_host": "vmx2.lasthop.io"},
+    "nxos1": {"ansible_host": "nxos1.lasthop.io"},
+    "nxos2": {"ansible_host": "nxos2.lasthop.io"},
 }
 
 
